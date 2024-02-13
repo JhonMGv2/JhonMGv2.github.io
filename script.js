06a4b8d39dd7d0571f195318e3ecef5491d3fa98
JhonMGv2 <kurstquencher123@gmail.com>
2024-02-12 21:01:43 -0500

    yeahyeah

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
-function confirmProceed() {
-    var confirmMessage = confirm("Are you sure you want to proceed?");
-    if (confirmMessage) {
-      nextPage();
-    } else {
-      // If the user clicks "Cancel", ask again
-      confirmProceed();
-    }
-  }
-  
-  function nextPage() {
-    // Hide the current page
-    document.getElementById('page1').style.display = 'none';
-    // Proceed to the next page or perform any other action
-    // For now, let's just show an alert
-    alert("Proceeding to the next page!");
+// Initialize and add the map
+function initMap() {
+    // The location of where you met or had your first kiss
+    const location = { lat: YOUR_LATITUDE, lng: YOUR_LONGITUDE };
+    // The map, centered at the location
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 15,
+      center: location,
+    });
+    // The marker, positioned at the location
+    const marker = new google.maps.Marker({
+      position: location,
+      map: map,
+    });
   }
   
