8e4c49cf82978759c8fb4562f8a2034a026756ce
JhonMGv2 <kurstquencher123@gmail.com>
2024-02-12 21:11:19 -0500

    123

--- script.js
+++ script.js
@@ -1,24 +1,10 @@
 // Initialize and add the map
-function initMap() {
-    // The location of where you met or had your first kiss
-    const location = { lat: 49.021750, lng: 12.058500 }; // Updated coordinates
-    // The map, centered at the location
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 15,
-      center: location,
-    });
-    // The custom heart icon
-    const icon = {
-      url: "https://img.icons8.com/plasticine/2x/heart.png", // Heart icon URL
-      scaledSize: new google.maps.Size(50, 50), // Size of the heart icon
-      origin: new google.maps.Point(0, 0), // Position of the heart icon
-      anchor: new google.maps.Point(25, 50), // Position to attach the heart icon
-    };
-    // The marker, positioned at the location
-    const marker = new google.maps.Marker({
-      position: location,
-      map: map,
-      icon: icon, // Use the heart icon
-    });
-  }
-  
+var map = L.map('map').setView([49.021750, 12.058500], 15); // Set initial view and zoom level
+
+// Add the OpenStreetMap tile layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+}).addTo(map);
+
+// Add a marker at the location where you met or had your first kiss
+var marker = L.marker([49.021750, 12.058500]).addTo(map);
